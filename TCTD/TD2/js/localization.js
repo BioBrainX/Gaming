1334b23180839b462558241fa52d5cf3677bf1c0
BioBrainX <BioBrainX@Live.com>
2024-11-08 21:09:15 +0700

    attributes

--- TCTD/TD2/js/localization.js
+++ TCTD/TD2/js/localization.js
@@ -1,13 +1,35 @@
-const lang= {
+const lang = {
 en: {
-    CHD: 'Critical Hit Damage',
-    CHC: 'Critical Hit Chance',
-    HSD: 'HeadShot Damage',
+	// Attributes
+	// Offensives
+	CHC: 'Critical hit chance',
+	CHD: 'Critical hit damage',
+	DOC: 'Damage to target out of cover',
+	DTA: 'Damage to armor',
+	HLD: 'Health damage',
+	HSD: 'HeadShot damage',
+	ROF: 'Rate of fire',
+	WHL: 'Weapon handling',
+	WPD: 'Weapon damage',
+	// Defensives
+	ARG: 'Armour regeneration',
+	ARM: 'Armour',
+	HLT: 'Health',
+	HZP: 'Hazard protection',
+	XRS: 'Explosive resistance',
+	// Utility
+	ACC: 'Accuracy',
+	MAG: 'Magazine size',
+	OPR: 'Optimal range',
+	RLD: 'Reload speed',
+	SFX: 'Status effects',
+	SKD: 'Skill damage',
+	SKH: 'Skill haste',
+	SKR: 'Skill repair',
+	SKT: 'Skill tier',
+	STB: 'Stability',
+	SWP: 'Swap speed',
 },
-fr: {
-    
-},
-es: {
-    
-},
+fr: {},
+es: {},
 };
