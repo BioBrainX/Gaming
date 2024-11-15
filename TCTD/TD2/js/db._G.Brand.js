ac6f88ab981b01bc8f8e924e687a5a9beaf33cef
BioBrainX <BioBrainX@Live.com>
2024-11-15 18:44:48 +0700

    Formatting

--- TCTD/TD2/js/db._G.Brand.js
+++ TCTD/TD2/js/db._G.Brand.js
@@ -1,13 +1,16 @@
-Ûpð(TDdb._G.Brand, { // Gears
-511: [
-	{
-		[_G.g]: {
-			úniq: {
-				name: 'Deathgrips',
-				_A: [, { AOK: 0.15 }],
-			}
+Ûpð(TDdb._G.Brand, {
+	// Gears
+	511: [
+		{
+			[_G.g]: {
+				úniq: {
+					name: 'Deathgrips',
+					_A: [, { AOK: 0.15 }],
+				},
+			},
 		},
-	},
-	{ HLT: 0.1 }, { HIC: 0.2 }, { PHZ: 0.1 },
-],
+		{ HLT: 0.1 },
+		{ HIC: 0.2 },
+		{ PHZ: 0.1 },
+	],
 });
--- TCTD/TD2/js/db._G.Exotic.js
+++ TCTD/TD2/js/db._G.Exotic.js
@@ -1,10 +1,12 @@
-Ûpð(TDdb._G.Xotc,{
-Memento: [{
-	avail: _G|b,
-	_A: [_cor|_O, _cor|_D, _cor|_U],
-	mod: 1,
-	b: {
-		Talent: _ => { },
-	},
-}],
+Ûpð(TDdb._G.Xotc, {
+	Memento: [
+		{
+			avail: _G | b,
+			_A: [_cor | _O, _cor | _D, _cor | _U],
+			mod: 1,
+			b: {
+				Talent: (_) => {},
+			},
+		},
+	],
 });
