fc2e2f14abaed63f6ec314a5b31f15b70f712ebd
BioBrainX <BioBrainX@Live.com>
2024-11-19 23:54:07 +0700

    updates

--- TCTD/TD2/js/TD2.js
+++ TCTD/TD2/js/TD2.js
@@ -3,33 +3,37 @@ clogt('TD2')
 
 
 function Player(pId=0) {
-	const þ=TDdb,
-		_ITM=þ._ITM,
-		_P=þ.Players[pId],
-		methods= {
-			calcOutput:_=>{
-				
-			},
-			eqp:(itms={})=> {
-				for(const slot in itms) {
-					const _GId=itms[slot];
-					_P._G[slot]= _GId;
-				}
-				this.Set_A()
-			},
-			Set_A:_=> {
-				const _G_A=þ._ITM;
-				for(const _A in _G_A) {
-					plyr._A[_A]= _G_A[_A]
-				}
-			},
-			IncTtlDmg:v=> {
-				_P[pId]._B[1]+=v //
-			},
-			DecTtlDmg:v=> {
-				_P[pId]._B[1]-=v //
-			},
-		}
+	const
+	þ=TDdb,
+	_ITM=þ._ITM,
+	_P=þ.Players[pId],
+
+	methods= {
+		calcOutput:_=>{
+			
+		},
+		Eqp:(itms={},eqp=_T)=> {
+			for(const slot in itms) {
+				const _GId=itms[slot]
+				_P._G[slot]= _GId
+				þ._G[_GId]._T['on'+(!eqp?'Un':'')+'Eqp']()
+				this.Set_A(_GId)
+			}
+		},
+		UnEqp:(itms={})=>this.Eqp(itms,_F),
+		Set_A:_GId=> {
+			const _G_A=þ._ITM;
+			for(const _A in _G_A) {
+				plyr._A[_A] +=_G_A[_A]
+			}
+		},
+		IncTtlDmg:v=> {
+			_P._B[1] +=v //
+		},
+		DecTtlDmg:v=> {
+			_P._B[1] -=v //
+		},
+	}
 	return methods
 }
 
