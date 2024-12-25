c32dd203b173aca83d059513a253c19d6195698c
BioBrainX <BioBrainX@Live.com>
2024-12-26 02:38:05 +0700

    ratio calc

--- ONI/main.js
+++ ONI/main.js
@@ -2,44 +2,38 @@
 
 const get = {
 	Building: function (b) {
-		const building = db[b]
+		const building = db[b],
+			mass = 'g/s'
 		return {
-			io_ratio: function() {
-				const Total = {}
-				const io = ['consume', 'produce']
-				for (const i of io) {
-					const io_item = building[i]
-					Total[i] = { [`g/s`]: 0 }
-					for (const item in io_item) {
-						const io_data = io_item[item]
-						if (elements.includes(item)) {
-							Total[i][`g/s`] += io_data[`g/s`]
-							Total[i][item] = io_data[`g/s`]
+			Ratio: {
+				elements: function (a, b) {
+					const a2b = `${a} → ${b}`,
+						el = { consume: building.consume[a], produce: building.produce[b] }
+
+					if (building.ratio?.[a2b]) return building.ratio[a2b]
+					if (!building.consume[a] || !building.produce[b]) return
+					if (!building.ratio) building.ratio = {}
+					building.ratio[a2b] = el.consume[mass] / el.produce[mass]
+					return this
+				},
+				IO: function () {
+					const Total = {}
+					const io = ['consume', 'produce']
+					for (const i of io) {
+						const io_item = building[i]
+						Total[i] = { [mass]: 0 }
+						for (const item in io_item) {
+							const io_data = io_item[item]
+							if (elements.includes(item)) {
+								Total[i][mass] += io_data[mass]
+								Total[i][item] = io_data[mass]
+							}
 						}
 					}
-				}
-				get.Ratio(b).AlgaeToO2() //.tst()
-				return (Total.io_ratio = building.io_ratio =
-					Total.consume[`g/s`] / Total.produce[`g/s`])
-			},
-		}
-	},
-
-	Ratio: function (b) {
-		const building = db[b]
-		return {
-			AlgaeToO2: function () {
-				if (building.ratio?.[`Algae → O2`]) return building.ratio[`Algae → O2`]
-				if (!building.consume.Algae || !building.produce.O2) return
-				const Algae = building.consume.Algae[`g/s`]
-				const O2 = building.produce.O2[`g/s`]
-				if (!building.ratio) building.ratio = {}
-				building.ratio[`Algae → O2`] = Algae / O2
-				return this
-			},
-			tst: function () {
-				clog('tst')
-				return this
+					this.elements('Algae', 'O2')
+					return (Total.io_ratio = building.io_ratio =
+						Total.consume[mass] / Total.produce[mass])
+				},
 			},
 		}
 	},
