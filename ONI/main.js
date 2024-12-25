5166d86af9729d3cc31a1487ded7fbc03692e3fe
BioBrainX <BioBrainX@Live.com>
2024-12-25 23:40:55 +0700

    update ratio calc

--- ONI/main.js
+++ ONI/main.js
@@ -1,42 +1,46 @@
 ﻿function compare(a, b) {}
 
-function getBuilding(b) {
-	const building = db[b];
-	return {
-		io_ratio: () => {
-			const Total = calculateTotalIO(building);
-			getRatio(b).AlgaeToO2();
-			clog(Total);
-			return (Total.ratio = building.io_ratio = Total.consume[`g/s`] / Total.produce[`g/s`]);
-		},
-	};
-}
-
-function calculateTotalIO(building) {
-	const Total = {};
-	const io = ['consume', 'produce'];
-	for (const i of io) {
-		const io_item = building[i];
-		Total[i] = { [`g/s`]: 0 };
-		for (const item in io_item) {
-			const io_data = io_item[item];
-			if (elements.includes(item)) {
-				Total[i][`g/s`] += io_data[`g/s`];
-				Total[i][item] = io_data[`g/s`];
-			}
+const get = {
+	Building: function (b) {
+		const building = db[b]
+		return {
+			io_ratio: function() {
+				const Total = {}
+				const io = ['consume', 'produce']
+				for (const i of io) {
+					const io_item = building[i]
+					Total[i] = { [`g/s`]: 0 }
+					for (const item in io_item) {
+						const io_data = io_item[item]
+						if (elements.includes(item)) {
+							Total[i][`g/s`] += io_data[`g/s`]
+							Total[i][item] = io_data[`g/s`]
+						}
+					}
+				}
+				get.Ratio(b).AlgaeToO2() //.tst()
+				return (Total.io_ratio = building.io_ratio =
+					Total.consume[`g/s`] / Total.produce[`g/s`])
+			},
 		}
-	}
-	return Total;
-}
+	},
 
-function getRatio(building) {
-	return {
-		AlgaeToO2: () => {
-			if (db[building].ratio?.AlgaeToO2) return db[building].ratio.AlgaeToO2;
-			if (!db[building].consume.Algae) return;
-			const Algae = db[building].consume.Algae[`g/s`];
-			const O2 = db[building].produce.O2[`g/s`];
-			return (db[building].ratio = { AlgaeToO2: Algae / O2 });
-		},
-	};
+	Ratio: function (b) {
+		const building = db[b]
+		return {
+			AlgaeToO2: function () {
+				if (building.ratio?.[`Algae → O2`]) return building.ratio[`Algae → O2`]
+				if (!building.consume.Algae || !building.produce.O2) return
+				const Algae = building.consume.Algae[`g/s`]
+				const O2 = building.produce.O2[`g/s`]
+				if (!building.ratio) building.ratio = {}
+				building.ratio[`Algae → O2`] = Algae / O2
+				return this
+			},
+			tst: function () {
+				clog('tst')
+				return this
+			},
+		}
+	},
 }
