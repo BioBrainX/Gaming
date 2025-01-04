dd2b85b175d9b6d44f06765c61405e3c5f0edb05
BioBrainX <BioBrainX@Live.com>
2025-01-04 14:32:48 +0700

    Refactor element initialization and I/O processing in main.js

--- ONI/main.js
+++ ONI/main.js
@@ -1,4 +1,74 @@
 ﻿clogt('ONImain')
+;(function () {
+	for (const category in db.Elements)
+		for (const el in db.Elements[category])
+			db.Elements[el] = initEl(db.Elements[category], el, category)
+	for (const category of ['Foods', 'Compostable']) {
+		const elmts = [...db[category]]
+		db[category] = {}
+		for (const el of elmts)
+			db[category][el] = el.match(/Foods/) ? db.Foods : initEl(elmts, el, category)
+	}
+
+	function initEl(source, el, category) {
+		db[el] ? (source[el] = db[el]) : (db[el] = source[el])
+		is(db[el]).obj()
+			? db[el].category?.push(category) ?? (db[el].category = [category])
+			: (db[el] = { category: [category] })
+		return db[el]
+	}
+
+	for (const category in db.Buildings) xtractIOP(db.Buildings[category], category)
+	for (const category of ['Creatures', 'Plants']) xtractIOP(db[category], category)
+
+	function xtractIOP(data, category) {
+		for (const name in data) {
+			const i_o_p = data[name]
+			db[name] = i_o_p
+			db[name].category?.push(category) ?? (db[name].category = [category])
+			for (const iop in i_o_p) {
+				if (iop == 'category') continue
+				//consume,produce,properties
+				const Elmts = i_o_p[iop]
+				if (is(Elmts).obj())
+					for (const el in Elmts) {
+						const val = Elmts[el],
+							iopr = iop + (iop.match(/e$/) ? 'r' : ''),
+							els = Get(el).els
+
+						if (iop == 'consume')
+							for (const el_P in i_o_p.produce)
+								for (const el of els)
+									_.merge(db, {
+										[el_P]: { source: { [el]: { [name]: {} } } },
+										[el]: { transform: { [el_P]: { [name]: {} } } },
+									})
+
+						for (const el of els) setIOP(el)
+
+						function setIOP(el) {
+							db[el] ??= { [iopr]: {} }
+							db[el][iopr] ??= {}
+
+							if (is(val).bool()) {
+								db[el][iopr][val] ??= []
+								db[el][iopr][val].push(name)
+							} else {
+								db[el][iopr][name] = val
+								// set mass per sec from mass per Cycle
+								if (val['g/C'])
+									db[el][iopr][name]['g/s'] = val['g/C'] / ttlSecPerCycle
+								else if (val['kcal/C'])
+									db[el][iopr][name]['g/s'] =
+										val['kcal/C'] / foodPrMass / ttlSecPerCycle
+							}
+						}
+					}
+			}
+			Calc(name).Ratio.IO()
+		}
+	}
+})()
 
 function compare(a, b) {}
 
@@ -62,14 +132,8 @@ function Calc(t) {
 				if (!target.Ratio[a2b]) {
 					// && el.consume && el.produce)
 					target.Ratio[a2b] = el.produce[mass] / el.consume[mass]
-					const els = a?.match(/,/)
-						? a.split(/\s*,\s*/)
-						: a.match(/Compostable|Foods/)
-						? Object.keys(db[a])
-						: ø
-					if (els)
-						for (const el of els) db[b].source[el][t] = { Ratio: target.Ratio[a2b] }
-					else db[b].source[a][t] = { Ratio: target.Ratio[a2b] }
+					for (const el of Get(a).els)
+						db[el].transform[b][t] = db[b].source[el][t] = { Ratio: target.Ratio[a2b] }
 				}
 				// clog(target.Ratio[a2b])
 
@@ -82,7 +146,7 @@ function Calc(t) {
 					if (!target.Ratio[a2b]) {
 						// && target.consume[power] && target.produce[el])
 						target.Ratio[a2b] = target.produce[el][mass] / target.consume[power]
-						db[el].source[power][t] = { Ratio: target.Ratio[a2b] }
+						db[power].transform[el][t] = db[el].source[power][t] = { Ratio: target.Ratio[a2b] }
 					}
 					// clog(el, target.Ratio[a2b])
 
@@ -94,15 +158,8 @@ function Calc(t) {
 					if (!target.Ratio[a2b]) {
 						// && target.consume[el] && target.produce[power])
 						target.Ratio[a2b] = target.produce[power] / target.consume[a][mass]
-						const els = a?.match(/,/)
-							? a.split(/\s*,\s*/)
-							: a.match(/Compostable|Foods/)
-							? Object.keys(db[a])
-							: ø
-						if (els)
-							for (const el of els)
-								db[power].source[el][t] = { Ratio: target.Ratio[a2b] }
-						else db[power].source[a][t] = { Ratio: target.Ratio[a2b] }
+						for (const el of Get(a).els)
+							db[el].transform[power][t] = db[power].source[el][t] = { Ratio: target.Ratio[a2b] }
 					}
 					// clog(el, target.Ratio[a2b])
 
@@ -146,86 +203,23 @@ function Calc(t) {
 	return þ
 }
 
-;(function () {
-	for (const category in db.Elements)
-		for (const el in db.Elements[category])
-			db.Elements[el] = initEl(db.Elements[category], el, category)
-	for (const category of ['Foods', 'Compostable']) {
-		const elmts = [...db[category]]
-		db[category] = {}
-		for (const el of elmts)
-			db[category][el] = el.match(/Foods/) ? db.Foods : initEl(elmts, el, category)
-	}
-
-	function initEl(source, el, category) {
-		db[el] ? (source[el] = db[el]) : (db[el] = source[el])
-		is(db[el]).obj()
-			? db[el].category?.push(category) ?? (db[el].category = [category])
-			: (db[el] = { category: [category] })
-		return db[el]
-	}
-
-	for (const category in db.Buildings) xtractIOP(db.Buildings[category], category)
-	for (const category of ['Creatures', 'Plants']) xtractIOP(db[category], category)
-
-	function xtractIOP(data, category) {
-		for (const name in data) {
-			const i_o_p = data[name]
-			db[name] = i_o_p
-			db[name].category?.push(category) ?? (db[name].category = [category])
-			for (const iop in i_o_p) {
-				if (iop == 'category') continue
-				//consume,produce,properties
-				const Elmts = i_o_p[iop]
-				if (is(Elmts).obj())
-					for (const el in Elmts) {
-						const val = Elmts[el],
-							iopr = iop + (iop.match(/e$/) ? 'r' : '')
-
-						const els = el.match(/,/)
-							? el.split(/\s*,\s*/)
-							: el.match(/Compostable|Foods/)
-							? Object.keys(db[el])
-							: ø
-
-						if (iop == 'consume')
-							for (const el_P in i_o_p.produce) {
-								if (els) for (const e of els) initSource(e)
-								else initSource(el)
-
-								function initSource(el) {
-									db[el_P] ??= { source: { [el]: { [name]: {} } } }
-									db[el_P].source ??= { [el]: { [name]: {} } }
-									db[el_P].source[el] ??= { [name]: {} }
-								}
-							}
-
-						if (els) for (const e of els) setIOP(e)
-						else setIOP(el)
-
-						function setIOP(el) {
-							db[el] ??= { [iopr]: {} }
-							db[el][iopr] ??= {}
-
-							if (is(val).bool()) {
-								db[el][iopr][val] ??= []
-								db[el][iopr][val].push(name)
-							} else {
-								db[el][iopr][name] = val
-								// set mass per sec from mass per Cycle
-								if (val['g/C'])
-									db[el][iopr][name]['g/s'] = val['g/C'] / ttlSecPerCycle
-								else if (val['kcal/C'])
-									db[el][iopr][name]['g/s'] =
-										val['kcal/C'] / foodPrMass / ttlSecPerCycle
-							}
-						}
-					}
-			}
-			Calc(name).Ratio.IO()
+function Get(x) {
+	function els() {
+		const db_link = {
+			Compostable: db.Compostable,
+			Foods: db.Foods,
+			Gas: db.Elements.Gas,
+			Liquid: db.Elements.Liquid,
+			Solid: db.Elements.Solid,
 		}
+		return x?.match(/,/) ? x.split(/\s*,\s*/) : db_link[x] ? Object.keys(db_link[x]) : [x]
 	}
-})()
+	return {
+		els: els(),
+	}
+}
+
+// localStorage.setItem('ver','0a.00.01')
 
 clog(db)
 
