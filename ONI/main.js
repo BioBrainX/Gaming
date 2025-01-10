0726785c383e4f0366b1b49a24e5396375ef03c3
BioBrainX <BioBrainX@Live.com>
2025-01-10 10:06:49 +0700

    Refactor main.js to unify ratio property naming and enhance path tracing logic

--- ONI/main.js
+++ ONI/main.js
@@ -1,20 +1,60 @@
 ﻿clogt('ONImain')
+
+function Trace(a, b) {
+	const a2b = `${a} → ${b}`,
+		traceAccul = { path: [] },
+		pathTraced = []
+
+	if (db[a].trace?.[a2b]) return db[a].trace[a2b]
+	if (PathingProduction(a, b)) return Ûpð(db[a], { trace: { [a2b]: traceAccul } })
+
+	function PathingProduction(a, b) {
+		if (!db[a].category) return false
+		clog(a, db[a].category)
+		if (are(...db[a].category).any.eq('Buildings', 'Plants', 'Creatures')) {
+			for (const elProd in db[a].produce) {
+				clog(`produce: ${elProd}`)
+				if (elProd == b)
+					Ûpð(traceAccul, {
+						path: [a],
+						consume: db[a].consume,
+					})
+				else {
+					traceAccul.path.push(elProd)
+					if (!PathingProduction(elProd, b)) traceAccul.path.pop()
+				}
+			}
+		} else if (are(...db[a].category).any.eq('Elements', 'Foods')) {
+			for (const consumer in db[a].consumer) {
+				clog(`consumer: ${consumer}`)
+				if (!traceAccul.path.includes(consumer)) {
+					traceAccul.path.push(consumer)
+					if (!PathingProduction(consumer, b)) traceAccul.path.pop()
+				}
+			}
+		}
+
+		return is(traceAccul).nøl() ? traceAccul : false
+	}
+}
+
 ;(function () {
 	for (const mainCat of ['Elements', 'Buildings'])
 		for (const category in db[mainCat])
-			for (const el in db[mainCat][category]) initEl(db[mainCat][category], el, category) // db[mainCat][el] =
+			for (const el in db[mainCat][category])
+				initEl(db[mainCat][category], el, [mainCat, category]) // db[mainCat][el] =
 	for (const category of ['Creatures', 'Foods', 'Plants'])
-		for (const el in db[category]) initEl(db[category], el, category)
+		for (const el in db[category]) initEl(db[category], el, [category])
 	for (const category of ['Compostable']) {
 		const elmts = [...db[category]]
 		db[category] = {}
 		for (const el of elmts)
-			db[category][el] = el.match(/Foods/) ? db.Foods : initEl(elmts, el, category)
+			db[category][el] = el.match(/Foods/) ? db.Foods : initEl(elmts, el, [category])
 	}
 
 	function initEl(source, el, category) {
 		db[el] ? (source[el] = db[el]) : (db[el] = source[el])
-		return Ûpð(db[el], { category: [category] })
+		return Ûpð(db[el], { category: [...category] })
 	}
 
 	for (const category in db.Buildings) xtractIOP(db.Buildings[category])
@@ -76,7 +116,7 @@ function Calc(t) {
 		mass = 'g/s',
 		power = 'W/s',
 		heat = 'DTU/s'
-	target.Ratio ??= {}
+	target.ratio ??= {}
 	const þ = {
 		// Get: {
 		// 	el_data: function (io, el) {
@@ -105,12 +145,12 @@ function Calc(t) {
 							if (elProduce == heat) this.Power.toHeat(elConsume)
 							else this.Power.in(elProduce)
 						}
-					// } else if (elConsume == heat) {
-					// 	Total[io][heat] += target[io][heat]
-					// 	for (const elProduce in target.produce) {
-					// 		// clog(elProduce, 'produce')
-					// 		this.Heat.in(elProduce)
-					// 	}
+						// } else if (elConsume == heat) {
+						// 	Total[io][heat] += target[io][heat]
+						// 	for (const elProduce in target.produce) {
+						// 		// clog(elProduce, 'produce')
+						// 		this.Heat.in(elProduce)
+						// 	}
 					}
 				}
 				io = 'produce'
@@ -121,7 +161,7 @@ function Calc(t) {
 				return (
 					Total.produce[mass] &&
 					Total.consume[mass] &&
-					(target.Ratio.IO ??= Total.produce[mass] / Total.consume[mass])
+					(target.ratio.IO ??= Total.produce[mass] / Total.consume[mass])
 				)
 			},
 			elements: function (a, b) {
@@ -132,86 +172,89 @@ function Calc(t) {
 					}
 
 				// clog(a2b, t, db)
-				if (!target.Ratio[a2b]) {
+				if (!target.ratio[a2b]) {
 					// && el.consume && el.produce)
-					target.Ratio[a2b] = el.produce[mass] / el.consume[mass]
+					target.ratio[a2b] = el.produce[mass] / el.consume[mass]
 					for (const el of Get(a).els)
-						db[b].source[el][t] = db[el].transform[b][t] = target.Ratio[a2b]
+						db[b].source[el][t] = db[el].transform[b][t] = target.ratio[a2b]
 				}
-				// clog(target.Ratio[a2b])
+				// clog(target.ratio[a2b])
 
-				return target.Ratio[a2b] // this
+				return target.ratio[a2b] // this
 			},
 			Power: {
 				in: function (el) {
 					const a2b = `${power} → ${el}`
 
-					if (!target.Ratio[a2b]) {
+					if (!target.ratio[a2b]) {
 						// && target.consume[power] && target.produce[el])
-						target.Ratio[a2b] = target.produce[el][mass] / target.consume[power]
-						db[el].source[power][t] = db[power].transform[el][t] = target.Ratio[a2b]
+						target.ratio[a2b] = target.produce[el][mass] / target.consume[power]
+						db[el].source[power][t] = db[power].transform[el][t] = target.ratio[a2b]
 					}
-					// clog(el, target.Ratio[a2b])
+					// clog(el, target.ratio[a2b])
 
-					return target.Ratio[a2b] // this
+					return target.ratio[a2b] // this
 				},
 				out: function (a) {
 					const a2b = `${a} → ${power}`
 
-					if (!target.Ratio[a2b]) {
+					if (!target.ratio[a2b]) {
 						// && target.consume[el] && target.produce[power])
-						target.Ratio[a2b] = target.produce[power] / target.consume[a][mass]
+						target.ratio[a2b] = target.produce[power] / target.consume[a][mass]
 						for (const el of Get(a).els)
-							db[power].source[el][t] = db[el].transform[power][t] = target.Ratio[a2b]
+							db[power].source[el][t] = db[el].transform[power][t] = target.ratio[a2b]
 					}
-					// clog(el, target.Ratio[a2b])
+					// clog(el, target.ratio[a2b])
 
-					return target.Ratio[a2b] // this
+					return target.ratio[a2b] // this
 				},
 				toHeat: function () {
 					const a2b = `${power} → ${heat}`
 
-					if (!target.Ratio[a2b]) {
+					if (!target.ratio[a2b]) {
 						// && target.consume[power] && target.produce[heat])
-						target.Ratio[a2b] = target.produce[heat] / target.consume[power]
-						// db[power].source[heat][t] = db[heat].transform[power][t] = target.Ratio[a2b]
-						Ûpð(db[power], {
-							source: { [heat]: { [t]: target.Ratio[a2b] } } },
-							// [heat]: { transform: { [power]: { [t]: target.Ratio[a2b] } } },
+						target.ratio[a2b] = target.produce[heat] / target.consume[power]
+						// db[power].source[heat][t] = db[heat].transform[power][t] = target.ratio[a2b]
+						Ûpð(
+							db[power],
+							{
+								source: { [heat]: { [t]: target.ratio[a2b] } },
+							}
+							// [heat]: { transform: { [power]: { [t]: target.ratio[a2b] } } },
 						)
 					}
-					// clog(a2b, target.Ratio[a2b])
+					// clog(a2b, target.ratio[a2b])
 
-					return target.Ratio[a2b] // this
+					return target.ratio[a2b] // this
 				},
 			},
 			Heat: {
 				// in: function (a) {
 				// 	const a2b = `${heat} → ${a}`
 
-				// 	if (!target.Ratio[a2b]) {
+				// 	if (!target.ratio[a2b]) {
 				// 		// && target.consume[heat] && target.produce[el])
-				// 		target.Ratio[a2b] = target.produce[a][mass] / target.consume[heat]
+				// 		target.ratio[a2b] = target.produce[a][mass] / target.consume[heat]
 				// 		for (const el of Get(a).els)
-				// 			db[el].source[heat][t] = db[heat].transform[el][t] = target.Ratio[a2b]
+				// 			db[el].source[heat][t] = db[heat].transform[el][t] = target.ratio[a2b]
 				// 	}
 
-				// 	// clog(el, target.Ratio[a2b])
+				// 	// clog(el, target.ratio[a2b])
 
-				// 	return target.Ratio[a2b] // this
+				// 	return target.ratio[a2b] // this
 				// },
 				out: function (a) {
 					const a2b = `${a} → ${heat}`
 
-					if (!target.Ratio[a2b]) {
+					if (!target.ratio[a2b]) {
 						// && target.consume[el] && target.produce[heat])
-						target.Ratio[a2b] = target.produce[heat] / target.consume[a][mass]
+						target.ratio[a2b] = target.produce[heat] / target.consume[a][mass]
 						for (const el of Get(a).els)
-							db[heat].source[el][t] = db[el].transform[heat][t] = target.Ratio[a2b]
+							db[heat].source[el][t] = db[el].transform[heat][t] = target.ratio[a2b]
 					}
-					// clog(el, target.Ratio[a2b])
+					// clog(el, target.ratio[a2b])
 
-					return target.Ratio[a2b] // this
+					return target.ratio[a2b] // this
 				},
 			},
 		},
